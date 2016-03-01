99678107490ea30806bbb59848735a749d5bcdf0
ZackKupstow <ZackKupstow@roaming-240-38.host.udel.edu>
2016-03-01 02:52:05 -0500

    init commit

---
+++ src/main/java/base/Main.java
@@ -0,0 +1,12 @@
+package base;
+
+import org.apache.poi.ss.formula.functions.FinanceLib;
+
+public class Main {
+
+	public static void main(String[] args) {
+		TuitionCalc tuitionCost = new TuitionCalc();
+		TuitionCalc.TuitionCost();
+	}
+
+}
